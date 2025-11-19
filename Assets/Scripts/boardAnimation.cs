667558f3fa3f2caff7a7ae74476ed731f521238d
Fred-qub <fwilliamson01@qub.ac.uk>
2025-11-19 20:26:08 +0000

    made trail only activate above a certain speed

--- Assets/Scripts/boardAnimation.cs
+++ Assets/Scripts/boardAnimation.cs
@@ -6,19 +6,22 @@ public class boardAnimation : MonoBehaviour
 {
     public Transform board;
     public Rigidbody capsuleHitboxRB;
+    public TrailRenderer trail;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         board = GameObject.Find("board").transform;
         capsuleHitboxRB = GameObject.Find("capsuleHitbox").GetComponent<Rigidbody>();
+        trail = GameObject.Find("trailPoint").GetComponent<TrailRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Takes the current 
         Quaternion velRot = Quaternion.Euler(capsuleHitboxRB.linearVelocity.normalized);
         board.localRotation = Quaternion.Slerp(board.localRotation, velRot, 0.1f * Time.deltaTime);
+        trail.emitting = (capsuleHitboxRB.linearVelocity.magnitude >= 100f);
+
     }
 }
