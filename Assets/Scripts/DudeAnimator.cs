c1c5810a1d61a8a44e359cf0012159c1fa4ed368
Fred-qub <fwilliamson01@qub.ac.uk>
2025-12-02 21:29:28 +0000

    tried and failed to add spring effect to waist

--- Assets/Scripts/DudeAnimator.cs
+++ Assets/Scripts/DudeAnimator.cs
@@ -2,6 +2,11 @@ using UnityEngine;
 
 public class DudeAnimator : MonoBehaviour
 {
+    public Transform dudeThighL;
+    public Transform dudeThighR;
+    public Rigidbody capsuleHitboxRB;
+    public float Yoffset = 0f;
+    public float scale = 1f;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -13,5 +18,8 @@ public class DudeAnimator : MonoBehaviour
     void Update()
     {
 
+       // float scaledYVelocity = capsuleHitboxRB.linearVelocity.y * scale;
+        //dudeThighL.position = Vector3.Lerp(capsuleHitboxRB.position, new Vector3(capsuleHitboxRB.position.x, scaledYVelocity, capsuleHitboxRB.position.z), Time.deltaTime);
+        //dudeThighR.position = dudeThighL.position;
     }
 }
