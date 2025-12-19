cd3522b7e6ab5aac57f3bf1c7f878dca91b9419e
Fred-qub <fwilliamson01@qub.ac.uk>
2025-12-19 19:28:47 +0000

    main menu panels

--- Assets/Scripts/SceneManagerScript.cs
+++ Assets/Scripts/SceneManagerScript.cs
@@ -3,8 +3,32 @@ using UnityEngine.SceneManagement;
 
 public class SceneManagerScript : MonoBehaviour
 {
+    public GameObject titlePanel;
+    public GameObject controlGuidePanel;
+    public GameObject scoreBoardPanel;
     public void LoadScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
+    }
+
+    public void backToTitle()
+    {
+        titlePanel.SetActive(true);
+        controlGuidePanel.SetActive(false);
+        scoreBoardPanel.SetActive(false);
+    }
+
+    public void openControlGuide()
+    {
+        titlePanel.SetActive(false);
+        controlGuidePanel.SetActive(true);
+        scoreBoardPanel.SetActive(false);
+    }
+
+    public void openScoreboard()
+    {
+        titlePanel.SetActive(false);
+        controlGuidePanel.SetActive(false);
+        scoreBoardPanel.SetActive(true);
     }
 }
--- Assets/Scripts/boardAnimation.cs
+++ Assets/Scripts/boardAnimation.cs
@@ -4,22 +4,19 @@ using UnityEngine.Splines.Interpolators;
 
 public class boardAnimation : MonoBehaviour
 {
-    public Transform board;
-    public Rigidbody capsuleHitboxRB;
     public TrailRenderer trail;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        board = GameObject.Find("board").transform;
-        capsuleHitboxRB = GameObject.Find("capsuleHitbox").GetComponent<Rigidbody>();
         trail = GameObject.Find("trailPoint").GetComponent<TrailRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        trail.emitting = (capsuleHitboxRB.linearVelocity.magnitude >= 100f);
+        
+        trail.emitting = (playerController.instance.isBoosting);
 
     }
 }
